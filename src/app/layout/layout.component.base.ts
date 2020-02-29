3319626dfbe06ab59f08e9245680b4feb72411d9
Ozymandias <tomas.rav@gmail.com>
2020-02-29 08:41:31 +0200

    - Migrated app styles to scss - Styled sidedrawer

--- src/app/layout/layout.component.base.ts
+++ src/app/layout/layout.component.base.ts
@@ -11,7 +11,7 @@ export class Layout {
 	public title = "Ginger";
 	public sideLinks: ISideLink[] = [
 		{ title: "Library", icon: "\uF333", iconClass: "mdi-library-music", path: null, type: "header" },
-		{ title: "Now Playing", icon: "\uF02A", iconClass: "mdi-play-box", path: "/library/queue", type: "link" },
+		{ title: "Now Playing", icon: "\u{F02A5}", iconClass: "mdi-play-box", path: "/library/queue", type: "link" },
 		{ title: "Playlists", icon: "\uFC94", iconClass: "mdi-playlist-music", path: "/library/playlists", type: "link" },
 		{ title: "Artists", icon: "\uF802", iconClass: "mdi-artist", path: "/library/artists", type: "link" },
 		{ title: "Albums", icon: "\uF025", iconClass: "mdi-album", path: "/library/albums", type: "link" },
