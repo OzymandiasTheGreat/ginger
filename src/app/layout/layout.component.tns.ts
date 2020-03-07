3c740b26a435ec3f262a0470dbfabea85238da93
Ozymandias <tomas.rav@gmail.com>
2020-03-07 06:23:37 +0200

    - Sidebar on mobile is now the same as on web

--- src/app/layout/layout.component.tns.ts
+++ src/app/layout/layout.component.tns.ts
@@ -1,7 +1,11 @@
-import { Component, AfterViewInit, ViewChild } from "@angular/core";
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import { Event as RouterEvent, NavigationEnd } from "@angular/router";
+import { RouterExtensions } from "nativescript-angular/router";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular";
 
-import { Layout, ISideLink } from "@src/app/layout/layout.component.base";
+import { Layout } from "@src/app/layout/layout.component.base";
 
 
 @Component({
@@ -9,16 +13,30 @@ import { Layout, ISideLink } from "@src/app/layout/layout.component.base";
 	templateUrl: "./layout.component.html",
 	styleUrls: ["./layout.component.scss"]
 })
-export class LayoutComponent extends Layout implements AfterViewInit {
-	@ViewChild("sidebar", { static: true }) public sidebar: RadSideDrawerComponent;
+export class LayoutComponent extends Layout implements OnInit, OnDestroy, AfterViewInit {
+	@ViewChild("sidebar", { static: true }) public sidebar: ElementRef<RadSideDrawerComponent>;
+	public openHeader = [];
 
-	constructor() {
+	private ngUnsubscribe: Subject<void>;
+
+	constructor(private router: RouterExtensions) {
 		super();
+		this.ngUnsubscribe = new Subject<void>();
+	}
+
+	public ngOnInit() {
+		this.router.router.events.pipe(takeUntil(this.ngUnsubscribe))
+			.subscribe((event: RouterEvent) => {
+				if (event instanceof NavigationEnd) {
+					this.openHeader = [this.links.findIndex(({ items }) => items.some((link) => this.router.router.isActive(link.path, false)))];
+				}
+			});
+	}
+
+	public ngOnDestroy() {
+		this.ngUnsubscribe.next();
+		this.ngUnsubscribe.complete();
 	}
 
 	public ngAfterViewInit() {}
-
-	public templateSelector(item: ISideLink, index: number, items: any) {
-		return item.type;
-	}
 }
